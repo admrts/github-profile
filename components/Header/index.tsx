1ea47c2bf2caf256e04ba37b1b9c806515b633f2
admrts <alidemirtas.7@icloud.com>
2023-04-08 15:42:21 +0300

    add logo

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,9 +1,12 @@
 import { FC } from "react";
 import "./index.css";
+import Logo from "../../assets/logo.png";
+import Image from "next/image";
 
 const Header: FC = () => {
   return (
-    <header>
+    <header className="header">
+      <Image src={Logo} alt="logo" width={40} height={40} />
       <h1>Github Search</h1>
     </header>
   );
